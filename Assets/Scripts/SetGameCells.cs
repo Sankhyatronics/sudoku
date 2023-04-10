f02817caf41269e39586ab4e7a35e750b53cc9cd
Singh Shekhawat <ajit.singh-shekhawat@capgemini.com>
2023-04-10 09:53:27 +0200

    Working input button

--- Assets/Scripts/SetGameCells.cs
+++ Assets/Scripts/SetGameCells.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,13 +13,14 @@ public class SetGameCells : MonoBehaviour
     [SerializeField] float BoarderDistane = 8;
     [SerializeField] private int BoardSize;
     private GameObject[,] cells;
-    //private int selectedRow = -1;
-    //private int selectedCol = -1;
+    private int selectedRow = -1;
+    private int selectedCol = -1;
     private int[,] solvedBoard;
     private int[,] unsolvedBoard;
     public int numNonZeroVars = 22;
+    [SerializeField] Sprite[] numberSprits;
+    [SerializeField] Button[] inputButtons;
 
-   
     void Start()
     {
         var boardGenerator = new SudokuBoardGenerator();
@@ -33,7 +35,7 @@ public class SetGameCells : MonoBehaviour
         RectTransform panelReact = gameObject.GetComponent<RectTransform>();
         //Calculating cell size basedon screen width
         float panelWidth = panelReact.rect.width;
-        float cellWidth = (panelWidth / BoardSize) -4;
+        float cellWidth = (panelWidth / BoardSize) - 4;
 
         cells = new GameObject[BoardSize, BoardSize];
         RectTransform gameCellPrefebTransform = CellPreFeb.GetComponent<RectTransform>();
@@ -50,18 +52,22 @@ public class SetGameCells : MonoBehaviour
                 GameObject cell;
                 cell = Instantiate(CellPreFeb);
                 cell.transform.SetParent(gameObject.transform);
-                //      cells[row, col] = cell;
-                //if (unsolvedBoard[row, col] > 0)
-                //{
-                //    cells[row, col].GetComponentInChildren<Button>().interactable = false;
+                Button button = cell.GetComponentInChildren<Button>();
+                cells[row, col] = cell;
+                if (unsolvedBoard[row, col] > 0)
+                {
+                    button.GetComponentInChildren<Image>().sprite = numberSprits[unsolvedBoard[row, col]];
+                    button.interactable = false;
 
-                //}
-                //else
-                //{
-                //    int cellRow = row; // create new local variable and assign loop variable's value to it
-                //    int cellCol = col; //
-                //   cell.GetComponentInChildren<Button>().onClick.AddListener(() => SetSelectCell(cellRow, cellCol));
-                //}
+                }
+                else
+                {
+                    int cellRow = row; // create new local variable and assign loop variable's value to it
+                    int cellCol = col; //
+                    button.GetComponentInChildren<Image>().sprite = numberSprits[0];
+                    button.GetComponentsInChildren<Image>().Where(x => x.gameObject.name == "Lock").FirstOrDefault().enabled = false;
+                    button.onClick.AddListener(() => SetSelectCell(cellRow, cellCol));
+                }
 
                 gameCellPrefebTransform = cell.GetComponent<RectTransform>();
                 gameCellPrefebTransform.anchoredPosition = new Vector3(statrupXPosition, -statrupYPosition, 0);
@@ -79,6 +85,21 @@ public class SetGameCells : MonoBehaviour
                 statrupYPosition += cellWidth;
             // Resetting X position for new row
             statrupXPosition = (cellWidth / 2) + BoarderDistane;
+        }
+    }
+
+    private void SetSelectCell(int cellRow, int cellCol)
+    {
+        selectedRow = cellRow;
+        selectedCol = cellCol;
+    }
+
+    public void OnInput(int inputNumber)
+    {
+        if (selectedRow > -1)
+        {
+            Button button = cells[selectedRow, selectedCol].GetComponentInChildren<Button>();
+            button.GetComponentInChildren<Image>().sprite = numberSprits[inputNumber];
         }
     }
 }
